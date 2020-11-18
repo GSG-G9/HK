cc17329ee0a02ce57932e28c3b3d85a80eb3d435
Hassan El Najjar <35685437+hassanelnajjar@users.noreply.github.com>
2020-11-18 21:29:43 +0200

    Revert "add server file"

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const router = require('./router');
-
-const port = process.env.PORT || 9000;
-const server = http.createServer(router);
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running @ http://localhost:${port}`);
-});
