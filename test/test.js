6f842a68f94aa35301ea244e93bbf0e44f688b70
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-19 17:57:20 +0200

    test-some-routes-using-supertest

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-undef */
+const supertest = require('supertest');
 const { handleSearch } = require('../src/handler/handler');
+const router = require('../src/router');
 
 describe('Test handleSearch Function', () => {
   test('Should return object with key:result and value:array of all words match input word', () => {
@@ -14,5 +17,33 @@ describe('Test handleSearch Function', () => {
     const actual = handleSearch(inputWord, wordListArray);
     const expected = '{"result":[]}';
     expect(expected).toEqual(actual);
+  });
+});
+
+describe('Test Routers', () => {
+  test('GET / should return status code 200', (done) => {
+    supertest(router)
+      .get('/')
+      .expect(200, done);
+  });
+
+  test('GET /search should return status code 200', (done) => {
+    supertest(router)
+      .get('/')
+      .send({ text: 'a' })
+      .expect(200, done);
+  });
+
+  test('GET /unknown should return status code 404', (done) => {
+    supertest(router)
+      .get('/unknown')
+      .expect('Content-Type', /html/)
+      .expect(404, done);
+  });
+  test('GET /404.html should return status code 404', (done) => {
+    supertest(router)
+      .get('/unknown')
+      .expect('Content-Type', /html/)
+      .expect(404, done);
   });
 });
